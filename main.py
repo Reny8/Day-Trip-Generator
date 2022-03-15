eec0252cd0cadb4772d759415cbb80788881f143
Reny Reyes <renyreyes8@hotmail.com>
2022-03-15 15:39:39 -0400

    Make a universal loop function for all the list in main.py

--- main.py
+++ main.py
@@ -1,80 +1,45 @@
 import random
+destinations = ["Upstate New York", "Salem, Massachusetts", "Portland, Oregon", "Austin, Texas", "Baltimore, Maryland", "Jim Thorpe, Pennsylvania","Miami, Florida", "Phoenix, Arizona"]
+restaurants =["Vetri's Pizza", "Zoe's Kitchen", "Outback Steakhouse", "Olive Garden", "LaScala", "Mod's Pizza","Red Lobster", "The Cheesecake Factory"]
+transportations = ["bus", "subway", "Uber", "cab", "rent-a-car", "biking"]
+entertainments = ["movie", "concert", "hiking/biking Trail", "sporting event", "comic convention", "comedy club show"]
 def display_title():
     print("Welcome to the Day Trip Generator!")
-destinations = ["Upstate New York", "Salem, Massachusetts", "Portland, Oregon", "Austin, Texas", "Baltimore, Maryland", "Jim Thorpe, Pennsylvania","Miami, Florida", "Phoenix, Arizona"]
-def destination_choice():
-    random_destination = random.choice(destinations)
-    user_dest_reply = input(f"Your destination is {random_destination}. Do you like the choice? y/n: ").lower()
-    if user_dest_reply == "y":
-        return random_destination
-    while user_dest_reply == "n":
-        if user_dest_reply == "n":
-            random_destination = random.choice(destinations)
-            user_dest_reply = input(f"What about {random_destination}? y/n: ").lower()
-        if user_dest_reply == "y":
-            return random_destination
+def list_loop(list):
+    
+    random_pick = random.choice(list)
+    user_reply = input(f"How does {random_pick} sound for your? y/n: ").lower()
+    if user_reply == "y":
+        return random_pick 
+    while user_reply == "n":
+        if user_reply == "n":
+            random_pick = random.choice(list)
+            user_reply = input(f"What about {random_pick}? y/n: ").lower()
+        if user_reply == "y":
+            return random_pick
 
-transportations = ["bus", "subway", "Uber", "cab", "rent-a-car", "biking"]
-def transportation_choice():
-    random_transport = random.choice(transportations)
-    user_transport_reply = input(f"When you get there, your transportation method will be by {random_transport}. Do you like the choice? y/n: ").lower()
-    if user_transport_reply == "y":
-        return random_transport
-    while user_transport_reply == "n":
-        if user_transport_reply == "n":
-            random_transport = random.choice(transportations)
-            user_transport_reply = input(f"What about: {random_transport}? y/n: ")
-        if user_transport_reply == "y":
-            return random_transport
-
-restaurants =["Vetri's Pizza", "Zoe's Kitchen", "Outback Steakhouse", "Olive Garden", "LaScala", "Mod's Pizza","Red Lobster", "The Cheesecake Factory"]
-def restaurant_choice():
-    random_restaurant = random.choice(restaurants)
-    user_rest = input(f"Your restaurant will be: {random_restaurant}. Do you like the choice? y/n: ").lower()
-    if user_rest == "y":
-        return random_restaurant 
-    while user_rest == "n":
-        if user_rest == "n":
-            random_restaurant = random.choice(restaurants)
-            user_rest = input(f"What about {random_restaurant}? y/n: ").lower()
-        if user_rest == "y":
-            return random_restaurant
-
-entertainments = ["movie", "concert", "hiking/biking Trail", "sporting event", "comic convention", "comedy club show"]
-def entertainment_choice():
-    random_entertain = random.choice(entertainments)
-    user_entertain = input(f"Your entertainment will be a {random_entertain}. Do you like the choice? y/n: ").lower()
-    if user_entertain == "y":
-        return random_entertain
-    while user_entertain == "n":
-        if user_entertain == "n":
-            random_entertain = random.choice(entertainments)
-            user_entertain = input (f"What about: {random_entertain}? y/n: ")
-        if user_entertain == "y":
-            return random_entertain
-
-def complete(dest,rest,trans,entertain):
+def complete(dest, rest, trans, entertain):
     satisfied = input("Are you satisfied with all your choices? y/n: ").lower()
     if satisfied == "y":
         print(f"You will enjoy a day trip at {dest} where you will enjoy a meal at {rest}. You will use {trans} to get there. You will enjoy a {entertain}!")
     while satisfied == "n":
         change = input("Type 1 for change destination, 2 for restaurant, 3 for transportation or 4 for entertainment. Press 0 for no more changes: ").lower()
         if change == "1":
-            dest = destination_choice()
+            dest = list_loop(dest)
         elif change == "2":
-            rest = restaurant_choice()
+            rest = list_loop(rest)
         elif change == "3":
-            trans = transportation_choice()
+            trans = list_loop(trans)
         elif change == "4":
-            entertain = entertainment_choice() 
+            entertain = list_loop(entertain)
         else:
             satisfied = input("Are you satisfied with your choices? y/n: ")
             print(f"You will enjoy a day trip at {dest} where you will enjoy a meal at {rest}. You will use {trans} to get there. You will enjoy a {entertain}!")
             return
 display_title()
-final_destination = destination_choice()
-final_restaurant = restaurant_choice()
-final_transportation = transportation_choice()
-final_entertainment = entertainment_choice()
-complete(final_destination,final_restaurant, final_transportation, final_entertainment)
+final_destination = list_loop(destinations)
+final_restaurant = list_loop(restaurants)
+final_transportation = list_loop(transportations)
+final_entertainment = list_loop(entertainments)
+complete(final_destination,final_restaurant,final_transportation,final_entertainment)
 
